966aef405328996c9dafde64463013a67a37b108
Max <nopprakorn.sarinsakul@gmail.com>
2019-08-13 11:59:21 +0700

    cipher

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
+// @EnableEncryptableProperties
 public class DemoApplication {
 
 	public static void main(String[] args) {
